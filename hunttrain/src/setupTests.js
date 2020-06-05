f8f5b5e809181c81a1247540966de0d470f45ed2
AEBus <AEBus@users.noreply.github.com>
2020-06-05 04:56:05 +0300

    0

--- hunttrain/src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import * as StyledComponents from './styled-components';
-
-configure({ adapter: new Adapter() });
-
-global.StyledComponents = StyledComponents;
